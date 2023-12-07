37d79e98476a8be3c8385bab8ac0ce6f20d4d26b
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-12-07 15:49:13 +0100

    Universal Upgradable Smart Contract: Initializer

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -2,9 +2,32 @@
 pragma solidity ^0.8.20;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is UUPSUpgradeable {
+//! storage is stored in the proxy, NOT the implementation
+
+//4 Proxy (STORAGE, borrowing funcs from) -> implementation
+//5 Proxy -> deploy implementation -> call some "initializer" function
+
+contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+        // owner = msg.sender
+    }
+
+    /**
+     * @notice initialize is like constructor for proxy
+     */
+    function initialize() public initializer {
+        // whatever logic needed
+        //! will modify the proxy contract's storage
+        __Ownable_init(msg.sender); //? sets owner to param
+        __UUPSUpgradeable_init();
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,8 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract BoxV2 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+        // owner = msg.sender
+    }
+
+    /**
+     * @notice initialize is like constructor for proxy
+     */
+    function initialize() public initializer {
+        // whatever logic needed
+        //! will modify the proxy contract's storage
+        __Ownable_init(msg.sender); //? sets owner to param
+        __UUPSUpgradeable_init();
+    }
 
     function setNumber(uint256 _number) external {}
 
@@ -13,4 +33,11 @@ contract BoxV2 {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    /**
+     *
+     * @param newImplementation The new implementation address
+     * @notice IF NOT IMPLEMENTED, MEANS ANYONE CAN AUTHORIZE AN UPGRADE
+     */
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
